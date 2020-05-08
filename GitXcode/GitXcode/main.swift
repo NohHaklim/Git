56a19dc83ad0786fed9a968778984c0d39a41f4f
NohHaklim <62823216+NohHaklim@users.noreply.github.com>
2020-05-09 00:20:46 +0900

    Updates 2 on Github only

--- GitXcode/GitXcode/main.swift
+++ GitXcode/GitXcode/main.swift
@@ -19,4 +19,7 @@ print("Hello World 4")
 print("Hello World 5")
 
 print("Hello World Updates on Github")
+
 print("Hello World Updates on Local Master Branch")
+
+print("Hello World Updates 2 on Github")
